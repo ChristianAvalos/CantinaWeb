173aa42bd0cfaa1003feab2ed246ea1bc409a14c
Christian <christian_avalos@mef.gov.py>
2026-07-20 14:54:54 -0300

    areglo de cabecera

--- CantinaWeb/src/components/Header.jsx
+++ CantinaWeb/src/components/Header.jsx
@@ -85,7 +85,7 @@ export default function Header({ onToggleSidebar }) {
                 onClick={toggleDropdown}
                 className="inline-flex items-center gap-2 rounded-xl bg-white/10 px-3 py-2 font-semibold transition hover:bg-white/15"
               >
-                <span>{user?.name}</span>
+                <span>{ user?.name.toUpperCase() }</span>
                 <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
                 </svg>
